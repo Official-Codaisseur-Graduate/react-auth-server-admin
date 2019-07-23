33744f1c1a51403d9701fcde0fa281c66402bb46
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-07-23 10:20:25 +0200

    Add store and reducer files

---
+++ src/components/SignUp/SignupContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SignupContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SignupContainer
