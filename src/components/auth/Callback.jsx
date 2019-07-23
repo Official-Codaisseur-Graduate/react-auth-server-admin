faf0ae29ec7eab2841ec93f5880a64c361a0f4fe
Billy Vlachos <williamcaminiti@live.com>
2019-07-23 22:30:39 +0200

    Add callback component"

---
+++ src/components/auth/Callback.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { AuthConsumer } from "../../providers/authProvider";
+
+/**
+ * callback: This component is going to be used 
+ * after login when the user is going to be 
+ * redirected by the server to the redirect_uri 
+ * setted on the IDENTITY_CONFIG
+ */
+export const Callback = () => (
+    <AuthConsumer>
+        {({ signinRedirectCallback }) => {
+            signinRedirectCallback();
+            return <span>loading</span>;
+        }}
+    </AuthConsumer>
+);
