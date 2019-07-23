d75ef3d25a337a14ebe95ed3a33b0de03aec9bf2
Billy Vlachos <williamcaminiti@live.com>
2019-07-23 22:24:01 +0200

    Configure auth constants

--- src/utils/authConst.js
+++ src/utils/authConst.js
@@ -1,27 +1,40 @@
+
+const authority = process.env.REACT_APP_AUTH_URL || "http://localhost:5000";
+
+const authConfig = {
+    reactAppAuthUrl: authority,
+    clientId: process.env.REACT_APP_IDENTITY_CLIENT_ID || "admin",
+    redirectUri: process.env.REACT_APP_REDIRECT_URL || "http://localhost:3000",
+    login: authority + "/auth",
+    silentRedirectUri: process.env.REACT_APP_SILENT_REDIRECT_URL || "",
+    postLogoutRedirectUri: process.env.REACT_APP_LOGOFF_REDIRECT_URL || "",
+
+}
+
 export const IDENTITY_CONFIG = {
-    authority: process.env.REACT_APP_AUTH_URL, //(string): The URL of the OIDC provider.
-    client_id: process.env.REACT_APP_IDENTITY_CLIENT_ID, //(string): Your client application's identifier as registered with the OIDC provider.
-    redirect_uri: process.env.REACT_APP_REDIRECT_URL, //The URI of your client application to receive a response from the OIDC provider.
-    login: process.env.REACT_APP_AUTH_URL + "/login",
+    authority: authConfig.reactAppAuthUrl, //(string): The URL of the OIDC provider.
+    client_id: authConfig.clientId, //(string): Your client application's identifier as registered with the OIDC provider.
+    redirect_uri: authConfig.redirectUri, //The URI of your client application to receive a response from the OIDC provider.
+    login: authConfig.login,
     automaticSilentRenew: false, //(boolean, default: false): Flag to indicate if there should be an automatic attempt to renew the access token prior to its expiration.
     loadUserInfo: false, //(boolean, default: true): Flag to control if additional identity data is loaded from the user info endpoint in order to populate the user's profile.
-    silent_redirect_uri: process.env.REACT_APP_SILENT_REDIRECT_URL, //(string): The URL for the page containing the code handling the silent renew.
-    post_logout_redirect_uri: process.env.REACT_APP_LOGOFF_REDIRECT_URL, // (string): The OIDC post-logout redirect URI.
-    audience: "https://example.com", //is there a way to specific the audience when making the jwt
+    silent_redirect_uri: authConfig.silentRedirectUri, //(string): The URL for the page containing the code handling the silent renew.
+    post_logout_redirect_uri: authConfig.postLogoutRedirectUri, // (string): The OIDC post-logout redirect URI.
+    audience: "admin", //is there a way to specific the audience when making the jwt
     responseType: "id_token token", //(string, default: 'id_token'): The type of response desired from the OIDC provider.
     grantType: "password",
-    scope: "openid example.api", //(string, default: 'openid'): The scope being requested from the OIDC provider.
+    scope: "openid profile admin", //(string, default: 'openid'): The scope being requested from the OIDC provider.
     webAuthResponseType: "id_token token"
 };
 
 export const METADATA_OIDC = {
     issuer: "https://identityserver",
-    jwks_uri: process.env.REACT_APP_AUTH_URL + "/.well-known/openid-configuration/jwks",
-    authorization_endpoint: process.env.REACT_APP_AUTH_URL + "/connect/authorize",
-    token_endpoint: process.env.REACT_APP_AUTH_URL + "/connect/token",
-    userinfo_endpoint: process.env.REACT_APP_AUTH_URL + "/connect/userinfo",
-    end_session_endpoint: process.env.REACT_APP_AUTH_URL + "/connect/endsession",
-    check_session_iframe: process.env.REACT_APP_AUTH_URL + "/connect/checksession",
-    revocation_endpoint: process.env.REACT_APP_AUTH_URL + "/connect/revocation",
-    introspection_endpoint: process.env.REACT_APP_AUTH_URL + "/connect/introspect"
+    jwks_uri: authConfig.reactAppAuthUrl + "/jwks",
+    authorization_endpoint: authConfig.reactAppAuthUrl + "/connect/authorize",
+    token_endpoint: authConfig.reactAppAuthUrl + "/connect/token",
+    userinfo_endpoint: authConfig.reactAppAuthUrl + "/connect/userinfo",
+    end_session_endpoint: authConfig.reactAppAuthUrl + "/connect/endsession",
+    check_session_iframe: authConfig.reactAppAuthUrl + "/connect/checksession",
+    revocation_endpoint: authConfig.reactAppAuthUrl + "/connect/revocation",
+    introspection_endpoint: authConfig.reactAppAuthUrl + "/connect/introspect"
 };
