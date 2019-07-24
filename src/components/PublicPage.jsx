1e550158fe339c27fe6cb81424fefd585854a0d5
Billy Vlachos <williamcaminiti@lvie.com>
2019-07-24 14:04:59 +0200

    Add a login button to the main public container

--- src/components/PublicPage.jsx
+++ src/components/PublicPage.jsx
@@ -19,7 +19,13 @@ export default class PublicPage extends Component {
             <div className="public-page">
                 <div className="public-inner-container">
                     <h1>PUBLIC PAGE</h1>
-                    <button onClick={this.login}>Login</button>
+                    <p>
+                        This is the public page. Anybody can have access to this page.
+                    </p>
+                    <p>
+                        If you would like to access the protected page, click the button below to login with our Auth Provider
+                    </p>
+                    <button onClick={this.login}>Login with Codaisseur</button>
                 </div>
             </div>
         )
