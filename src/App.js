d1ed6b4d4cd1d5c05684f10caee17e0ab50db1a0
Billy Vlachos <williamcaminiti@lvie.com>
2019-07-24 15:32:05 +0200

    Add header and css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Routes } from "./routes/Routes";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./providers/authProvider";
 import './App.css';
+import HeaderContainer from "./components/header/HeaderContainer";
 
 class App extends Component {
   render() {
     return (
       <AuthProvider>
-        
+        <HeaderContainer />
         <BrowserRouter children={Routes} basename={"/"} />
       </AuthProvider>
     );
