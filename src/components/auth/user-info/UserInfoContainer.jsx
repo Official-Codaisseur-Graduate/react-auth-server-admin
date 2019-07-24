f2889d5a0ff2164e97707cd062616bbbe240e2c5
Billy Vlachos <williamcaminiti@lvie.com>
2019-07-24 15:46:26 +0200

    Add user info

---
+++ src/components/auth/user-info/UserInfoContainer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserInfoContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
