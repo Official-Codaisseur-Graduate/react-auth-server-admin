1d369351e52557b775e3ccb6537b51d574c9272e
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-07-26 10:37:41 +0200

    Logout feature under the users info

--- src/components/nav/NavContainer.jsx
+++ src/components/nav/NavContainer.jsx
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import "./NavContainer.css";
-import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
+import {Menu, MenuItem, Button, ListItemIcon } from '@material-ui/core';
 
-export default class NavContainer extends Component {
-    render() {
-        return (
-            <List component="nav" >
-                <ListItem component="div">
-                    <ListItemText inset>
-                        <Typography>
-                            
-                        </Typography>
-                    </ListItemText>
-                </ListItem>
-            </List>
+export default function NavContainer(){
+    const [anchorEl, setAnchorEl] = React.useState(null)
+
+    function handleClick(event) {
+        console.log('set',setAnchorEl)
+        setAnchorEl(event.currentTarget)
+    }
+
+    function handleClose(){
+        console.log('normal', anchorEl)
+        setAnchorEl(null)
+    }
+        return (<div>
+            <Button
+            aria-controls="simpl-menu" 
+            onClick={handleClick}
+            >
+            Menu
+            </Button>
+            <Menu id="menu"
+                keepMounted
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Signup      </MenuItem>
+                <MenuItem onClick={handleClose}>Log In      </MenuItem>
+            </Menu>
+        </div>
         )
     }
-}
