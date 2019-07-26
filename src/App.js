d20f28cd1228e71cf06b0cc6bf5fc5017af26025
Billy Vlachos <williamcaminiti@lvie.com>
2019-07-26 15:14:25 +0200

    fix app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Routes } from "./routes/Routes";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./providers/authProvider";
 import './App.css';
-import HeaderContainer from "./components/header/HeaderContainer";
-import FooterContainer from "./components/footer/FooterContainer";
 
 class App extends Component {
   render() {
