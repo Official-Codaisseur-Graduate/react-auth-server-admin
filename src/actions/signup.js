9308ac7be2f458ff7a06bf3fdc64ceeb16842a9c
Kamaal Farah <kamaal.f1@gmail.com>
2019-08-27 11:47:59 +0200

    !

--- src/actions/signup.js
+++ src/actions/signup.js
@@ -1,10 +1,17 @@
-import * as request from 'superagent'
-export const SIGN_IN_INFO = 'SIGN_IN_INFO'
+import * as request from 'superagent';
+export const SIGN_IN_INFO = 'SIGN_IN_INFO';
 
-const baseUrl = process.env.REACT_APP_AUTH_URL || 'https://172.16.31.64:5000'
-const thisUrl = 'http://localhost:3000'
+const baseUrl = process.env.REACT_APP_AUTH_URL || 'http://172.16.31.138:5000';
+const thisUrl = 'http://localhost:3000';
 
-export function actionSignUp(email, firstName, lastName, password, gender, dateOfBirth) {
+export function actionSignUp(
+    email,
+    firstName,
+    lastName,
+    password,
+    gender,
+    dateOfBirth
+) {
     return {
         type: SIGN_IN_INFO,
         payload: {
@@ -15,16 +22,16 @@ export function actionSignUp(email, firstName, lastName, password, gender, dateO
             gender: gender,
             dateOfBirth: dateOfBirth
         }
-    }
+    };
 }
 
-export const signUp = (data) => (dispatch) => {
-    request
-        .post(`${baseUrl}/users/signup`)
-        .send({ ...data })
-        .then(res => {
-            if (res.status === 201) {
-                window.history.pushState(`${thisUrl}/register-conferm`)
-            }
-        })
-}
+export const signUp = data => async dispatch => {
+    const res = await request.post(`${baseUrl}/users/signup`).send({ ...data });
+    // if (res.status === 201) {
+    //     window.history.pushState(`${thisUrl}/register-conferm`);
+    // }
+
+    const action = actionSignUp(res.body);
+
+    return dispatch(action);
+};
--- src/components/SignUp/SignUpForm.js
+++ src/components/SignUp/SignUpForm.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import TextField from '@material-ui/core/TextField'
-import Select from '@material-ui/core/Select'
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { MenuItem, InputLabel } from '@material-ui/core';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export class SignUpForm extends Component {
     state = {
@@ -15,16 +15,16 @@ export class SignUpForm extends Component {
         passwordConfirmation: '',
         gender: '',
         dateOfBirth: ''
-    }
+    };
 
-    onSubmit = (event) => {
-        event.preventDefault()
+    onSubmit = event => {
+        event.preventDefault();
         if (this.state.password !== this.state.passwordConfirmation) {
-            alert('Password must be identical')
+            alert('Password must be identical');
         }
 
         //Send data to SignUpContainer.js
-        this.props.onSubmit(this.state)
+        this.props.onSubmit(this.state);
 
         //Reset state
         this.setState({
@@ -35,21 +35,25 @@ export class SignUpForm extends Component {
             passwordConfirmation: '',
             gender: '',
             dateOfBirth: ''
-        })
-    }
+        });
+    };
 
-    onChange = (event) => {
-        const { name, value } = event.target
+    onChange = event => {
+        const { name, value } = event.target;
 
         //Set the state property's to the value typed in
         this.setState({
             [name]: value
-        })
-    }
+        });
+    };
     render() {
         return (
             <div>
-                <form className="signUpForm" noValidate onSubmit={this.onSubmit}>
+                <form
+                    className="signUpForm"
+                    noValidate
+                    onSubmit={this.onSubmit}
+                >
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
                             <TextField
@@ -144,14 +148,14 @@ export class SignUpForm extends Component {
                         variant="contained"
                         color="primary"
                         className="signUpButton"
-                        component={Link} to={"/login"}
+                        // component={Link} to={"/login"}
                     >
                         Sign Up
                     </Button>
                 </form>
             </div>
-        )
+        );
     }
 }
 
-export default SignUpForm
+export default SignUpForm;
