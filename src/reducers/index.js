a165a579919d92a744ae909190afe2f949cf95fc
Kamaal Farah <kamaal.f1@gmail.com>
2019-08-30 10:21:12 +0200

    Added error handling in the reducer actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,4 +3,6 @@ import { combineReducers } from 'redux';
 import signup from './signup';
 import users from './users';
 
-export default combineReducers({ signup, users });
+import errors from './errors';
+
+export default combineReducers({ signup, users, errors });
